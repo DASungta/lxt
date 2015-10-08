d6a2252dd698639a47fcfd0d9be3c8559832b9e6
DASungta <z.y.c.7777@163.com>
2015-10-08 22:29:58 +0800

    Revert "lxt-beta"
    This reverts commit 11ae98bbcf27cdb6299620891b7350edd7b70901.

--- lxt/Home/Common/function.php
+++
@@ -1,9 +0,0 @@
-<?php
- function checkStatus(){
-        $cash=D('Home/Cash');
-        $c_s=$cash->getStatus(session('mid'));
-        //p($c_s);die;
-        $e_s=D('Exchange')->getStatus(session('mid'));
-        if($c_s==0||$e_s==0) return true;
-        else return false;
-    }
